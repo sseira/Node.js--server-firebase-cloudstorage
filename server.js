cf8266e89f7204a6e90b125422b3a9b3046ccc04
Santiago Seira <sseira@ideo.com>
2017-04-18 17:33:20 -0700

    added firebase

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 var express = require('express');
 var app = express();
-var https = require('https');
-var fs = require('fs');
-
 
 app.set('port', (process.env.PORT || 5000));
-
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // eventually only allow from IDEO domains???
@@ -21,8 +17,6 @@ app.use(function(req, res, next) {
 
     // assumes you previously added a view.html file in same directory
 /* ---------------------------------------------------------------------------- */
-
-
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
@@ -58,8 +52,10 @@ var getParameters = function(request) {
   return  params
 }
 
-var findParameterByKey = function(params, key) {
-  return params.find(function(param_obj) { return Object.keys(param_obj)[0] === key })
+var findParameterByKey = function(params, key) {  
+  var found_obj = params.find(function(param_obj) { return Object.keys(param_obj)[0] === key })
+
+  return found_obj[key]
 }
 
 var combineParameters = function(param1, param2) {
@@ -76,10 +72,10 @@ var sendGetRequest = function(base_url, paramaters) {
 /* --------------- Call External API ---------------- 
   best to keep sensitive data such as API keys in the server side
 */
+var https = require('https')
 
 app.get('/external_api', function(request, response) {
 
-console.log('requesting')
   var example_recipe_app_id = 'c5bd1e1a',
       example_recipe_key = '2b746487bce0eb83675174a4429c1a94',
       // server_params = [{app_id:example_recipe_app_id}, {app_key:example_recipe_key}]
@@ -93,45 +89,179 @@ console.log('requesting')
   // combine user and server parameters
 
   https.get(url, (res) => {
-     const statusCode = res.statusCode;
-     const contentType = res.headers['content-type'];
+     const statusCode = res.statusCode
+     const contentType = res.headers['content-type']
       
       //-------- error handling ------------
-     var error;
+     var error
      if (statusCode !== 200) {
        error = new Error(`Request Failed.\n` +
-                         `Status Code: ${statusCode}`);
+                         `Status Code: ${statusCode}`)
      } else if (!/^application\/json/.test(contentType)) {
        error = new Error(`Invalid content-type.\n` +
-                         `Expected application/json but received ${contentType}`);
+                         `Expected application/json but received ${contentType}`)
      }
      if (error) {
-       console.log(error.message);
+       console.log(error.message)
        // consume response data to free up memory
-       res.resume();
-       return;
+       res.resume()
+       return
      }
  
      //-------- reading in data chunk by chunk ------------
 
-     res.setEncoding('utf8');
-     var rawData = '';
-     res.on('data', (chunk) => rawData += chunk);
+     res.setEncoding('utf8')
+     var rawData = ''
+     res.on('data', (chunk) => rawData += chunk)
 
      //-------- finished reading data ------------
      res.on('end', () => {
        try {
-         var parsedData = JSON.parse(rawData);
-         response.json(parsedData);
-         console.log(parsedData);
+         var parsedData = JSON.parse(rawData)
+         response.json(parsedData)
+         console.log(parsedData)
        } catch (e) {
-         console.log(e.message);
+         console.log(e.message)
        }
      });
   }).on('error', (e) => {
-     console.log(`Got error: ${e.message}`);
+     console.log(`Got error: ${e.message}`)
   });
- 
- 
+})
+
+
+
+
+
+/*--------------- Access Firebase Database ---------------- */
+var firebase = require("firebase")
+
+var initializeFirebase = function() {
+  var config = {
+    apiKey: "AIzaSyC-dEU8rrYcPZjIyiD5paZFaWGLrGSjK4Q",
+    authDomain: "exampledatabase-b0ae3.firebaseapp.com",
+    databaseURL: "https://exampledatabase-b0ae3.firebaseio.com",
+    storageBucket: "exampledatabase-b0ae3.appspot.com",
+  }
+
+  firebase.initializeApp(config)
+
+  var listenForChanges = function(db_table) {
+    var tableRef = firebase.database().ref('users/');
+
+    tableRef.on('value', function(snapshot) {
+      doSomethingBasedOnData(snapshot.val());
+    });
+  }('users')
+
+
+}()
+
+// TODO
+// explore authentication
+// explore notifications
+
+var doSomethingBasedOnData = function(newData) {
+  console.log('finally')
+  console.log(newData)
+}
+
+app.get('/database', function(request, response) {
+
+  function writeUserData(userID, name, data) {
+    firebase.database().ref('users/' + userID).set({
+      username: name,
+      data: data
+    })
+    response.end()
+  }
+
+
+  function readUserData(userID) {
+    var data = firebase.database().ref('/users/' + userID).once('value').then(function(snapshot) {
+      var user = snapshot.val(),
+          username = user.username
+
+      doSomethingBasedOnData(username)
+      response.end()
+    });
+  }
+
+  var database = firebase.database(),
+      params = getParameters(request),
+      userID = findParameterByKey(params, 'userID'),
+      username = findParameterByKey(params, 'username'),
+      data = findParameterByKey(params, 'data')
+      
+
+  // writeUserData(userID, username, data)
+  readUserData(userID)
 
 })
+
+
+
+
+// http://localhost:5000/database?username=sana&data=lotsofdata&userID=111
+
+
+
+
+
+
+
+
+/*--------------- Get Google Directions ----------------
+
+  Google Directions API blocks your key if it is called too much
+  Also, modify algorithm to get simple A->B directions for next stop and B->C->...->X for the rest of stops
+  Traffic data is only available for A->B queries
+
+  If cloning, get your own key for any of these services https://developers.google.com/maps/web-services/overview 
+
+            ------ from Google Directions API Docs -----------
+The duration in traffic is returned only if all of the following are true:
+
+The request does not include stopover waypoints. 
+If the request includes waypoints, they must be prefixed with via: to avoid stopovers.
+
+If you'd like to influence the route using waypoints without adding a stopover, 
+prefix the waypoint with via:. Waypoints prefixed with via: will not add an entry to the legs
+ array, but will instead route the journey through the provided waypoint.
+
+*/
+var googleMapsDirectionsClient = require('@google/maps').createClient({
+  key: 'AIzaSyDhYQs3y6neWPf1TIX_Y8IgjTtOcpSe7X0'//'AIzaSyDVl65wW5zqkICh0c1UrabLIn4MV8ryIfk'
+})
+
+
+app.get('/googleDirections', function(request, response) {
+
+  var params = getParameters(request),
+      origin = findParameterByKey(params, 'origin'),
+      destination = findParameterByKey(params, 'destination')
+
+  googleMapsDirectionsClient.directions({
+    origin: origin,
+    // waypoints: waypoints,
+    destination: destination, 
+    departure_time: 'now', 
+    traffic_model: 'best_guess'
+  }, function(err, data) {    
+    if (!err) {
+
+      response.json(data.json)
+
+    } else {
+      console.log('google direction api err-' + err)
+    }
+  })
+
+})
+
+
+
+
+
+
+
